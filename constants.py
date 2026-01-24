ab1ef975784420b12f5cec617379173a279cdc94
Mika Novakovic <mochimoto@hotmail.com>
2026-01-25 02:28:31 +0900

    now it's an actual bot

--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
 SHREK_PATH = './data/shrek.txt'
 MATRIX_PATH = './matrix/shrek.pkl'
+TOKEN_PATH = './token.txt'
+
+COOLDOWN_SECONDS = 5
--- main.py
+++ main.py
@@ -1,18 +1,47 @@
 import random
+import time
 
-from constants import MATRIX_PATH
+import discord
+
+from constants import MATRIX_PATH, TOKEN_PATH, COOLDOWN_SECONDS
 from matrix import load_matrix
 from generator import generate_sentence
 
 
-if __name__ == '__main__':
+matrix = load_matrix(MATRIX_PATH)
+all_SOL = [key for key in matrix.keys() if key[0] == '[SOL]'] # all starting words
 
-    matrix = load_matrix(MATRIX_PATH)
+# discord stuff
+with open(TOKEN_PATH, 'r', encoding='utf8') as f:
+    token = f.read()
 
-    all_SOL = [key for key in matrix.keys() if key[0] == '[SOL]']
+intents = discord.Intents.default()
+intents.message_content = True
 
-    # generate some random sentences
-    for _ in range (10):
+client = discord.Client(intents=intents)
+
+last_response_time = 0
+
+@client.event
+async def on_ready():
+    print(f'logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    global last_response_time
+
+    if message.author == client.user:
+        return
+
+    if client.user.mentioned_in(message):
+        current_time = time.time()
+
+        if current_time - last_response_time < COOLDOWN_SECONDS:
+            return
+
         starting_words = all_SOL[random.randint(0, len(all_SOL) - 1)]
         output = generate_sentence(matrix, starting_words, temperature=10)
-        print(output)
+
+        await message.reply(output)
+
+client.run(token)
