319bd74897586b85b119e5f07e45c316abca4087
Mika Novakovic <mochimoto@hotmail.com>
2026-01-30 04:01:42 +0900

    fixed small error in punctuation processing of shrek script

--- shrek_processor.py
+++ shrek_processor.py
@@ -14,9 +14,9 @@ def process_shrek(path: str) -> str:
     with open(path, mode='r', encoding='utf8') as f:
         for line in list(f)[2: 3011]: # ignore SHREK SCRIPT and the ending songs
             # line = re.sub(r'\{.*?\}', '', line) # comment this bit out if you want to include the {} bracketed bits
-            line = re.sub(r'\.{2,}', ' .', line)
-            line = re.sub(r'\?{2,}', ' ?', line)
-            line = re.sub(r'\!{2,}', ' !', line)
+            line = re.sub(r'\.{2,}', '.', line)
+            line = re.sub(r'\?{2,}', '?', line)
+            line = re.sub(r'\!{2,}', '!', line)
             line = line.replace('\n', '')
             line = line.replace(' -', '')
             line = line.replace('-', '')
