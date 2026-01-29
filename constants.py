b56656d2a4062f0096efe7124d54b2e5c3bfe9eb
Mika Novakovic <mochimoto@hotmail.com>
2026-01-30 03:45:32 +0900

    added extra documentation

--- constants.py
+++ constants.py
@@ -5,8 +5,8 @@ SHREK_PATH = './data/shrek.txt'
 MATRIX_PATH = './matrix/shrek_word_n2.pkl'
 TOKEN_PATH = './token.txt'
 
-TOKENIZER = WordTokenizer
-MATRIX_N = 2
-TEMPERATURE = 10
+TOKENIZER = WordTokenizer # which tokenizer? WordTokenizer tokenizes each words, CharacterTokenizer tokenizes each letters
+MATRIX_N = 2 # number of tokens used as key in matrix
+TEMPERATURE = 10 # basically randomness out output, higher the more chaotic
 
-COOLDOWN_SECONDS = 5
+COOLDOWN_SECONDS = 5 # bot cool down period in seconds. so people can't spam the bot
